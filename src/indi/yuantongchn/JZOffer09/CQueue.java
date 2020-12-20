9384f3438da1c4fa4d5ae84dfaa3f2612bbf6bc1
YuanTongCHN <yuantongchn@outlook.com>
2020-12-20 22:04:48 +0800

    version 1.0

---
+++ src/indi/yuantongchn/JZOffer09/CQueue.java
@@ -0,0 +1,30 @@
+package indi.yuantongchn.JZOffer09;
+
+import java.util.LinkedList;
+
+public class CQueue {
+    LinkedList<Integer> addStack;
+    LinkedList<Integer> removeStack;
+    public CQueue() {
+        addStack = new LinkedList<>();
+        removeStack = new LinkedList<>();
+    }
+
+    public void appendTail(int value) {
+        addStack.addLast(value);
+    }
+
+    public int deleteHead() {
+        if (removeStack.isEmpty() && addStack.isEmpty()) {
+            return -1;
+        }
+        if (!removeStack.isEmpty()) {
+            return removeStack.removeLast();
+        }else {
+            while (!addStack.isEmpty()) {
+                removeStack.addLast(addStack.removeLast());
+            }
+            return removeStack.removeLast();
+        }
+    }
+}
