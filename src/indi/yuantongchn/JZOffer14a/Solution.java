c5f49c0a998b13a0b1d899b1b7001d2b8de934d5
YuanTongCHN <yuantongchn@outlook.com>
2021-01-03 11:38:01 +0800

    version 1.0

--- src/indi/yuantongchn/JZOffer14a/Solution.java
+++ src/indi/yuantongchn/JZOffer14a/Solution.java
@@ -21,16 +21,13 @@ package indi.yuantongchn.JZOffer14a;
 public class Solution {
     public int cuttingRope(int n) {
         //m、n都是整数，n > 1并且m > 1
-        if (n < 2)
-        {
+        if (n < 2) {
             return 0;
         }
-        if (n == 2)
-        {
+        if (n == 2) {
             return 1;
         }
-        if (n == 3)
-        {
+        if (n == 3) {
             return 2;
         }
         int[] k = new int[n + 1];
@@ -38,10 +35,8 @@ public class Solution {
         k[1] = 1;
         k[2] = 2;
         k[3] = 3;
-        for(int i = 4; i <= n; i++)
-        {
-            for(int j = 1; j <= i / 2; j++)
-            {
+        for (int i = 4; i <= n; i++) {
+            for (int j = 1; j <= i / 2; j++) {
                 k[i] = Math.max(k[i], k[j] * k[i - j]);
             }
         }
