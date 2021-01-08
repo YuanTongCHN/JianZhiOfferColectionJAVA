ac297764308d7f584c6786f8a0c3f7f54d7fb397
YuanTongCHN <yuantongchn@outlook.com>
2021-01-08 19:57:44 +0800

    version 1.0

---
+++ src/indi/yuantongchn/JZOffer49/Solution.java
@@ -0,0 +1,29 @@
+package indi.yuantongchn.JZOffer49;
+
+//丑数 是只包括2，3，5质因子的数
+
+public class Solution {
+    public int nthUglyNumber(int n) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            int n2 = dp[a] * 2;
+            int n3 = dp[b] * 3;
+            int n5 = dp[c] * 5;
+            dp[i] = Math.min(Math.min(n2, n3), n5);
+            if (dp[i] == n2) {
+                a++;
+            }
+            if (dp[i] == n3) {
+                b++;
+            }
+            if (dp[i] == n5){
+                c++;
+            }
+        }
+        return dp[n - 1];
+    }
+}
