745f5aea1be38bdde4d9081a1251c036a468a22c
YuanTongCHN <yuantongchn@outlook.com>
2021-01-17 10:19:20 +0800

    version 1.0

---
+++ src/indi/yuantongchn/JZOffer55a/Solution.java
@@ -0,0 +1,23 @@
+package indi.yuantongchn.JZOffer55a;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + Math.max(left, right);
+    }
+}
